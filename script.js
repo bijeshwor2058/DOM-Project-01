b8184002f48823a4099841990949052bccb77912
Bijeshwor Sharma <bijeshwor2058@gmail.com>
2025-01-14 16:59:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let divContainer = document.getElementById('container')
+
+let text = document.getElementById('text')
+console.log(divContainer)
+
+divContainer.style.background="yellow";
+divContainer.style.width = '300px'
+divContainer.style.height = '200px'
+
+divContainer.style.margin = '20px'
+divContainer.style.padding = '10px'
+
+divContainer.style.fontSize = '18px'
+divContainer.style.color= 'blue'
+divContainer.style.fontWeight = 'bold'
